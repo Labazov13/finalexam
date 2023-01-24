175c35debe1011806e601637c96403b5f8370055
Nikolay <labazopelastra@gmail.com>
2023-01-24 16:14:54 +0700

    add new method FillArray

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 3;
+String[] array = new String[a];
+
+String[] FillArray()
+{
+    for (int i = 0; i <= a; i++)
+    {
+        Console.Write("Введите " + i + " элемент массива: ");
+        //String element=Console.ReadLine()!;
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+}
+
+
+
+
+
+
+
